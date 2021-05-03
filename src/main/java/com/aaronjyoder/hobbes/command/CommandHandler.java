779d3179db5e34e76d2c329ddb12734f06c195b4
Aaron Yoder <aaronjyoder@gmail.com>
2021-05-03 05:00:14 -0400

    Add getAuthenticator method to CommandHandler

--- src/main/java/com/aaronjyoder/hobbes/command/CommandHandler.java
+++ src/main/java/com/aaronjyoder/hobbes/command/CommandHandler.java
@@ -1,10 +1,13 @@
 package com.aaronjyoder.hobbes.command;
 
+import com.aaronjyoder.hobbes.auth.Authenticator;
 import com.aaronjyoder.hobbes.input.Input;
 import java.util.Set;
 import javax.annotation.Nonnull;
 
 public interface CommandHandler<T> {
+
+  Authenticator getAuthenticator();
 
   void registerCommands(@Nonnull final Set<Command<T>> commands);
 
